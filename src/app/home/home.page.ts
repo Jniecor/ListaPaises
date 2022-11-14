b8f7906dc58d4a012b2590f06aece09ece899543
jniecor <jniecor515@g.educaand.es>
2022-11-14 10:23:39 +0100

    JNC. Primera Revision. Subida de archivos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  countries: any;
+  filtro: string = '';
+
+  constructor(private httpClient: HttpClient) {
+
+    this.countries = this.httpClient.get('https://raw.githubusercontent.com/Jniecor/listaPaises-JSON/master/MOCK_DATA.json');
+
+  }
 
 }
